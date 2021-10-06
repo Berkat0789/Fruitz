8ebcfc683ea974b45c8dadd4021ba1448ae072f6
Berkat Bhatti <berkat0789@gmail.com>
2021-10-06 18:11:56 -0400

    Initial Commit

---
+++ Fruitz/FruitzApp.swift
@@ -0,0 +1,17 @@
+//
+//  FruitzApp.swift
+//  Fruitz
+//
+//  Created by Yadilsa Diaz on 10/6/21.
+//
+
+import SwiftUI
+
+@main
+struct FruitzApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
